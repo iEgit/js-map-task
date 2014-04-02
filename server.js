383b0ebf80f40d4147a999b03095b1c7dde6cee7
Egor Kopylov <koegit@gmail.com>
2014-04-02 12:51:05 +0400

    added Procfile and some minor changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-    port = parseInt(process.argv[2], 10) || 8000;
+    port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
 
